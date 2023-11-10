6e60e85fdac76a95b5d63a91185171051f3704b0
Alosh Bennett <aloshbennett@gmail.com>
2023-11-10 22:13:41 +0000

    tool to sync dotfiles between git and config folders

---
+++ sync.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/bash
+
+usage() {
+    echo "Usage: sync.sh diff|pull|push"
+}
+
+
+if [ $# -lt 1 ]; then
+    usage
+    exit 0
+fi
+
+declare -a src=(
+    "i3/i3blocks.conf"
+    "i3/brightness.py"
+    "i3/block"
+    "i3/i3_multi_conf"
+    "i3/mouse_toggle"
+    "i3/cli_wrapper.sh"
+    "i3/config"
+    "sway/config"
+    "tmux/.tmux.conf"
+    "neovim/init.lua"
+)
+
+declare -a dst=(
+    "$HOME/.config/i3/i3blocks.conf"
+    "$HOME/.config/i3/brightness.py"
+    "$HOME/.config/i3/block"
+    "$HOME/.config/i3/i3_multi_conf"
+    "$HOME/.config/i3/mouse_toggle"
+    "$HOME/.config/i3/cli_wrapper.sh"
+    "$HOME/.config/i3/config"
+    "$HOME/.config/sway/config"
+    "$HOME/.tmux.conf"
+    "$HOME/.config/nvim/init.lua"
+)
+
+declare -A files
+
+fn_prepare() {
+    len=${#src[@]}
+    for (( i=0; i<$len; i++ )); do
+        fs=${src[$i]}
+        fd=${dst[$i]}
+        if [ ! -f $fd ]; then
+            continue;
+        fi
+        diff $fs $fd > /dev/null
+        if (( $? )); then
+            files["$fs"]=$fd
+        fi
+    done
+}
+
+fn_diff() {
+    for src in "${!files[@]}"; do
+        dst=${files[$src]}
+        echo "$src -> $dst"
+        read -p "Repo file '$src' different from '$dst'. See diff? [Y/n]" yn
+        case $yn in
+            [Nn]* )
+                ;;
+            * )
+                diff --color=always $src $dst | less -r
+                ;;
+        esac
+    done
+}
+
+fn_pull() {
+    for src in "${!files[@]}"; do
+        dst=${files[$src]}
+        echo "$src -> $dst"
+        read -p "Pull file '$src' from  repo and overwrite '$dst'? [Y/n]" yn
+        case $yn in
+            [Nn]* )
+                ;;
+            * )
+                // cp $src $dst
+                ;;
+        esac
+    done
+}
+
+fn_push() {
+    for src in "${!files[@]}"; do
+        dst=${files[$src]}
+        echo "$src -> $dst"
+        read -p "Push file '$dst' to repo? [Y/n]" yn
+        case $yn in
+            [Nn]* )
+                ;;
+            * )
+                cp $dst $src
+                ;;
+        esac
+    done
+}
+
+fn_prepare
+case $1 in
+    diff )
+        fn_diff
+        ;;
+    pull )
+        fn_pull
+        ;;
+    push )
+        fn_push
+        ;;
+    * )
+        usage
+        ;;
+esac
+
